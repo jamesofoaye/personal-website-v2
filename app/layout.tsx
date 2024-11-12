0fc5c09355eee937bbc02996c705c6114aa574a9
James Ofori Ayerakwa <58654469+jamesofoaye@users.noreply.github.com>
2024-11-13 02:11:01 +0400

    Refactor SEO_METADATA to use Object.freeze for immutability and simplify RootLayoutProps type definition

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,11 @@ import "./globals.css";
 import { fonts } from "./chakra-config";
 import { Provider } from "@/components/ui/provider"
 
-const SEO_METADATA = {
+const SEO_METADATA = Object.freeze({
   title: "Ofori James Ayerakwa",
   description: "A Frontend Engineer with a strong eye for innovative design and a keen understanding of techniques geared toward optimum user experience. I'm passionate about continuously learning in the tech industry and supporting people to solve their technical problems.",
   image: "https://ofori-james-ayerakwa.me/og-image.jpg",
-}
+})
 
 export const metadata: Metadata = {
   title: SEO_METADATA.title,
@@ -27,9 +27,7 @@ export const metadata: Metadata = {
   }
 };
 
-type RootLayoutProps = {
-  children: ReactNode;
-};
+type RootLayoutProps = { children: ReactNode };
 
 const RootLayout: FC<RootLayoutProps> = ({ children }) => {
   return (
